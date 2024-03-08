4d8eaef61e93507275d1d0589a721bb941a52085
Lucas Gabriel Daltro Lopes <lucasfbr123@gmail.com>
2024-03-08 09:13:01 -0300

    Definir o NAVBAR como um component

---
+++ resources/views/components/nav.blade.php
@@ -0,0 +1,30 @@
+<nav class="navbar navbar-expand-lg bg-primary">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="{{ asset('images/verbo_logo.png') }}" alt="" style="height: 30px"></a>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active link-light" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link-light" href="#">Feed</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle link-light" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                        Escalas
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+            </ul>
+            <a class="btn btn-success me-3">Entrar</a>
+            <a class="btn btn-secondary">Cadastrar</a>
+        </div>
+    </div>
+</nav>
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,24 +1,9 @@
 @extends('layouts.default')
 
-@section('title', 'Bem-vindo a Verbowweb')
+@section('title', 'Bem-vindo a VerboWeb')
 
 @section('content')
-    <nav class="navbar navbar-expand-lg" style="background-color: #020BF0">
-        <div class="navbar-brand ms-5">
-            <a href="" class="link-light link-underline-opacity-0">Foto</a>
-        </div>
 
-        <div class="mx-auto">
-            <a href="" class="link-light link-underline-opacity-0 me-3">Home</a>
-            <a href="" class="link-light link-underline-opacity-0 me-3">feed</a>
-            <a href="" class="link-light link-underline-opacity-0 me-3">about</a>
-            <a href="" class="link-light link-underline-opacity-0 me-3">escalas</a>
-        </div>
+@include('components.nav')
 
-        <div class="me-2">
-            <a href="" class="btn btn-light">Entrar</a>
-            <a href="" class="btn btn-outline-light">Cadastrar</a>
-        </div>
-
-    </nav>
 @endsection
